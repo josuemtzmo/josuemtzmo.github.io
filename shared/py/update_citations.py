f19dea9401aeee0f1337eb6fb7cccee3f81b47c6
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-18 19:00:02 +1000

    Add CI to update publications.

---
+++ shared/py/update_citations.py
@@ -0,0 +1,30 @@
+from scholarly import scholarly
+import subprocess
+import os
+
+search_query = scholarly.search_author('Josue Martinez Moreno')
+data = scholarly.fill(next(search_query), sections=['basics', 'indices', 'publications'])
+
+citedby = data['citedby']
+publications = len(data['publications'])
+
+def replace_string(line_identifier, replacement_string):
+    cmd = [
+            "sed",
+            "-i.bk",
+            "s/<\s.*{0}.*/<span class=\"w3-xlarge\">{1}<\/span><br>{0}/g".format(line_identifier,replacement_string),
+            "research.html"
+        ]
+    return cmd
+
+replace_lines = {"<!--Cit-->": citedby, "<!--Pub-->": publications}
+
+for key,item in replace_lines.items():
+    cmd = replace_string(key, item)
+
+    proc = subprocess.run(
+        cmd
+    )
+
+if os.path.isfile('./research.html.bk'):
+    os.remove('./research.html.bk')
