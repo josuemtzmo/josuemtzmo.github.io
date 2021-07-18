7eb60b6eff3e813277758fb84f9b91620d1ef74f
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-18 21:55:11 +1000

    Test fix in docker container

--- shared/py/update_citations.py
+++ shared/py/update_citations.py
@@ -1,6 +1,7 @@
 from scholarly import scholarly
 import subprocess
 import os
+import platform
 
 search_query = scholarly.search_author('Josue Martinez Moreno')
 data = scholarly.fill(next(search_query), sections=['basics', 'indices', 'publications'])
@@ -8,10 +9,14 @@ data = scholarly.fill(next(search_query), sections=['basics', 'indices', 'public
 citedby = data['citedby']
 publications = len(data['publications'])
 
+sed_edit = ''
+if platform.system() == "Darwin":
+    sed_edit = '.bk'
+
 def replace_string(line_identifier, replacement_string):
     cmd = [
             "sed",
-            "-i.bk",
+            "-i{0}".format(sed_edit),
             "s/<.*{0}.*/<span class=\"w3-xlarge\">{1}<\/span><br>{0}/g".format(line_identifier,replacement_string),
             "research.html"
         ]
@@ -26,3 +31,6 @@ for key,item in replace_lines.items():
     proc = subprocess.run(
         cmd
     )
+
+if os.path.isfile('./research.html.bk'):
+    os.remove('./research.html.bk')
