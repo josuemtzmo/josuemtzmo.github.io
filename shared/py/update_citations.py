0fcc70c7a14719d8604367baca4a8705f6602c3e
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2021-07-18 20:34:18 +1000

    Condition within one job

--- shared/py/update_citations.py
+++ shared/py/update_citations.py
@@ -11,7 +11,8 @@ publications = len(data['publications'])
 def replace_string(line_identifier, replacement_string):
     cmd = [
             "sed",
-            "-i.bk",
+            "-i",
+            # "-i.bk", # Uncoment for macOS
             "s/<\s.*{0}.*/<span class=\"w3-xlarge\">{1}<\/span><br>{0}/g".format(line_identifier,replacement_string),
             "research.html"
         ]
